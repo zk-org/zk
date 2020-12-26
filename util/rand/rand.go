3bfc0754c7bfd3b2c1ab17a15441fee5abf0ff59
Mickaël Menu <mickael.menu@gmail.com>
2020-12-26 15:14:31 +0100

    Generate random IDs

--- util/rand/rand.go
+++ util/rand/rand.go
@@ -1,14 +1,20 @@
 package rand
 
+import (
+	"math/rand"
+	"time"
+	"unicode"
+)
+
 var (
 	// AlphanumCharset is a charset containing letters and numbers.
 	AlphanumCharset = []rune("0123456789abcdefghijklmnopqrstuvwxyz")
 	// AlphanumCharset is a charset containing hexadecimal characters.
 	HexCharset = []rune("0123456789abcdef")
 	// LettersCharset is a charset containing only letters.
-	LettersCharset = []rune("0123456789abcdefghijklmnopqrstuvwxyz")
+	LettersCharset = []rune("abcdefghijklmnopqrstuvwxyz")
 	// NumbersCharset is a charset containing only numbers.
-	NumbersCharset = []rune("0123456789abcdefghijklmnopqrstuvwxyz")
+	NumbersCharset = []rune("0123456789")
 )
 
 // Case represents the letter case to use when generating a string.
@@ -22,16 +28,38 @@ const (
 
 // IDOpts holds the options used to generate a random ID.
 type IDOpts struct {
-	Charset []rune
 	Length  int
+	Charset []rune
 	Case    Case
 }
 
 // GenID creates a new random string ID using the given options.
+// Inspired by https://www.calhoun.io/creating-random-strings-in-go/
 func GenID(options IDOpts) string {
 	if options.Length < 1 {
 		panic("IDOpts.Length must be at least 1")
 	}
 
-	return ""
+	var charset []rune
+	for _, char := range options.Charset {
+		switch options.Case {
+		case LowerCase:
+			charset = append(charset, unicode.ToLower(char))
+		case UpperCase:
+			charset = append(charset, unicode.ToUpper(char))
+		case MixedCase:
+			charset = append(charset, unicode.ToLower(char))
+			charset = append(charset, unicode.ToUpper(char))
+		default:
+			panic("unknown rand.Case value")
+		}
+	}
+
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	buf := make([]rune, options.Length)
+	for i := range buf {
+		buf[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(buf)
 }
