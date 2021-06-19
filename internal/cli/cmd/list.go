6efa46ad43e588c0cf5f8a3ed21ec4b2164f9080
Mickaël Menu <mickael.menu@gmail.com>
2021-06-19 22:51:23 +0200

    Add json and jsonl list formats Add header and footer list options

--- internal/cli/cmd/list.go
+++ internal/cli/cmd/list.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
 
 	"github.com/mickael-menu/zk/internal/adapter/fzf"
 	"github.com/mickael-menu/zk/internal/cli"
 	"github.com/mickael-menu/zk/internal/util/errors"
-	strutil "github.com/mickael-menu/zk/internal/util/strings"
+	"github.com/mickael-menu/zk/internal/util/strings"
 )
 
 // List displays notes matching a set of criteria.
 type List struct {
 	Format     string `group:format short:f placeholder:TEMPLATE   help:"Pretty print the list using the given format."`
+	Header     string `group:format                                help:"Arbitrary text printed at the start of the list."`
+	Footer     string `group:format                                help:"Arbitrary text printed at the end of the list."`
 	Delimiter  string "group:format short:d default:\n             help:\"Print notes delimited by the given separator.\""
 	Delimiter0 bool   "group:format short:0 name:delimiter0        help:\"Print notes delimited by ASCII NUL characters. This is useful when used in conjunction with `xargs -0`.\""
 	NoPager    bool   `group:format short:P help:"Do not pipe output into a pager."`
@@ -23,8 +24,49 @@ type List struct {
 }
 
 func (cmd *List) Run(container *cli.Container) error {
+	cmd.Header = strings.ExpandWhitespaceLiterals(cmd.Header)
+	cmd.Footer = strings.ExpandWhitespaceLiterals(cmd.Footer)
+	cmd.Delimiter = strings.ExpandWhitespaceLiterals(cmd.Delimiter)
+
 	if cmd.Delimiter0 {
+		if cmd.Delimiter != "\n" {
+			return errors.New("--delimiter and --delimiter0 can't be used together")
+		}
+		if cmd.Header != "" {
+			return errors.New("--footer and --delimiter0 can't be used together")
+		}
+		if cmd.Footer != "" {
+			return errors.New("--footer and --delimiter0 can't be used together")
+		}
+
 		cmd.Delimiter = "\x00"
+		cmd.Footer = "\x00"
+	}
+
+	if cmd.Format == "json" || cmd.Format == "jsonl" {
+		if cmd.Header != "" {
+			return errors.New("--header can't be used with JSON format")
+		}
+		if cmd.Footer != "" {
+			return errors.New("--footer can't be used with JSON format")
+		}
+		if cmd.Delimiter != "\n" {
+			return errors.New("--delimiter can't be used with JSON format")
+		}
+
+		switch cmd.Format {
+		case "json":
+			cmd.Delimiter = ","
+			cmd.Header = "["
+			cmd.Footer = "]\n"
+
+		case "jsonl":
+			// > The last character in the file may be a line separator, and it
+			// > will be treated the same as if there was no line separator
+			// > present.
+			// > https://jsonlines.org/
+			cmd.Footer = "\n"
+		}
 	}
 
 	notebook, err := container.CurrentNotebook()
@@ -64,6 +106,9 @@ func (cmd *List) Run(container *cli.Container) error {
 	count := len(notes)
 	if count > 0 {
 		err = container.Paginate(cmd.NoPager, func(out io.Writer) error {
+			if cmd.Header != "" {
+				fmt.Fprint(out, cmd.Header)
+			}
 			for i, note := range notes {
 				if i > 0 {
 					fmt.Fprint(out, cmd.Delimiter)
@@ -75,8 +120,8 @@ func (cmd *List) Run(container *cli.Container) error {
 				}
 				fmt.Fprint(out, ft)
 			}
-			if cmd.Delimiter0 {
-				fmt.Fprint(out, "\x00")
+			if cmd.Footer != "" {
+				fmt.Fprint(out, cmd.Footer)
 			}
 
 			return nil
@@ -84,7 +129,7 @@ func (cmd *List) Run(container *cli.Container) error {
 	}
 
 	if err == nil && !cmd.Quiet {
-		fmt.Fprintf(os.Stderr, "\n\nFound %d %s\n", count, strutil.Pluralize("note", count))
+		fmt.Fprintf(os.Stderr, "\n\nFound %d %s\n", count, strings.Pluralize("note", count))
 	}
 
 	return err
@@ -98,18 +143,17 @@ func (cmd *List) noteTemplate() string {
 
 	templ, ok := defaultNoteFormats[format]
 	if !ok {
-		templ = format
-		// Replace raw \n and \t by actual newlines and tabs in user format.
-		templ = strings.ReplaceAll(templ, "\\n", "\n")
-		templ = strings.ReplaceAll(templ, "\\t", "\t")
+		templ = strings.ExpandWhitespaceLiterals(format)
 	}
 
 	return templ
 }
 
 var defaultNoteFormats = map[string]string{
-	"path": `{{path}}`,
-	"link": `{{link}}`,
+	"json":  `{{json .}}`,
+	"jsonl": `{{json .}}`,
+	"path":  `{{path}}`,
+	"link":  `{{link}}`,
 
 	"oneline": `{{style "title" title}} {{style "path" path}} ({{date created "elapsed"}})`,
 
