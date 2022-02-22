83c15cc9278351dccff4ff1dbeb4a852415d69d3
codito <pratikarun@gmail.com>
2022-02-22 19:03:30 +0530

    Fix Windows build (#171)

--- internal/util/exec/exec_windows.go
+++ internal/util/exec/exec_windows.go
@@ -3,15 +3,16 @@ package exec
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 	"syscall"
 )
 
 // CommandFromString returns a Cmd running the given command.
-func CommandFromString(command string) *exec.Cmd {
+func CommandFromString(command string, args ...string) *exec.Cmd {
 	cmd := exec.Command("cmd")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		HideWindow:    false,
-		CmdLine:       fmt.Sprintf(` /v:on/s/c "%s"`, command),
+		CmdLine:       fmt.Sprintf(` /v:on/s/c "%s %s"`, command, strings.Join(args[:], " ")),
 		CreationFlags: 0,
 	}
 	return cmd
