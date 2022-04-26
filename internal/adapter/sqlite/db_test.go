d8c9031f9c7b2bebe9eada6e52243d8403b34100
Mickaël Menu <mickael.menu@gmail.com>
2022-04-26 11:43:34 +0200

    Release zk 0.10.0

--- internal/adapter/sqlite/db_test.go
+++ internal/adapter/sqlite/db_test.go
@@ -27,7 +27,7 @@ func TestMigrateFrom0(t *testing.T) {
 		var version int
 		err := tx.QueryRow("PRAGMA user_version").Scan(&version)
 		assert.Nil(t, err)
-		assert.Equal(t, version, 6)
+		assert.Equal(t, version, 7)
 
 		_, err = tx.Exec(`
 			INSERT INTO notes (path, sortable_path, title, body, word_count, checksum)
