62131d9a7eacf36cf115b7ec431df8159ef050bd
Mickaël Menu <mickael.menu@gmail.com>
2022-07-10 08:31:48 +0200

    Support standard input via shell redirection with `zk new`

--- internal/util/os/os.go
+++ internal/util/os/os.go
@@ -1,33 +1,11 @@
 package os
 
 import (
-	"bufio"
-	"io/ioutil"
 	"os"
 	"strings"
 
 	"github.com/mickael-menu/zk/internal/util/opt"
 )
-
-// ReadStdinPipe returns the content of any piped input.
-func ReadStdinPipe() (opt.String, error) {
-	fi, err := os.Stdin.Stat()
-	if err != nil {
-		return opt.NullString, err
-	}
-	if fi.Mode()&os.ModeNamedPipe == 0 {
-		// Not a pipe
-		return opt.NullString, nil
-	}
-
-	reader := bufio.NewReader(os.Stdin)
-	bytes, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return opt.NullString, err
-	}
-
-	return opt.NewNotEmptyString(string(bytes)), nil
-}
 
 // Getenv returns an optional String for the environment variable with given
 // key.
