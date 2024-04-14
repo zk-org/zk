0973f9929dcfd65125ef942f935099b002e87683
Tillman Jex <tjex@tjex.net>
2024-04-14 21:34:22 +0200

    allow notebook as hidden dir (#402)

--- internal/util/paths/walk_test.go
+++ internal/util/paths/walk_test.go
@@ -16,8 +16,38 @@ func TestWalk(t *testing.T) {
 		return filepath.Ext(path) != ".md", nil
 	}
 
+	notebookRoot := filepath.Base(path)
 	actual := make([]string, 0)
-	for m := range Walk(path, &util.NullLogger, shouldIgnore) {
+	for m := range Walk(path, &util.NullLogger, notebookRoot, shouldIgnore) {
+		assert.NotNil(t, m.Modified)
+		actual = append(actual, m.Path)
+	}
+
+	assert.Equal(t, actual, []string{
+		"Dir3/a.md",
+		"a.md",
+		"b.md",
+		"dir1/a.md",
+		"dir1/b.md",
+		"dir1/dir1/a.md",
+		"dir1 a space/a.md",
+		"dir2/a.md",
+	})
+}
+
+// Walk should ignore all hidden files and dirs (prefixed with "."), with
+// exception of the notebook's root dir; i.e the root dir is allowed to be
+// hidden.
+func TestWalkHidden(t *testing.T) {
+	var path = fixtures.Path(".walk-hidden")
+
+	shouldIgnore := func(path string) (bool, error) {
+		return filepath.Ext(path) != ".md", nil
+	}
+
+	notebookRoot := filepath.Base(path)
+	actual := make([]string, 0)
+	for m := range Walk(path, &util.NullLogger, notebookRoot, shouldIgnore) {
 		assert.NotNil(t, m.Modified)
 		actual = append(actual, m.Path)
 	}
