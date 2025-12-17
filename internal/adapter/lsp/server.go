acc0d8287dfb78a07cffe899ca16f09bab3c613f
Michael McDonagh <michael@mcdonagh.dev>
2025-12-16 20:29:48 -0500

    Format file

--- internal/adapter/lsp/server.go
+++ internal/adapter/lsp/server.go
@@ -8,16 +8,16 @@ import (
 	"strings"
 	"time"
 
-	"github.com/zk-org/zk/internal/core"
-	"github.com/zk-org/zk/internal/util"
-	"github.com/zk-org/zk/internal/util/errors"
-	"github.com/zk-org/zk/internal/util/opt"
-	strutil "github.com/zk-org/zk/internal/util/strings"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 	glspserv "github.com/tliron/glsp/server"
 	"github.com/tliron/kutil/logging"
 	_ "github.com/tliron/kutil/logging/simple"
+	"github.com/zk-org/zk/internal/core"
+	"github.com/zk-org/zk/internal/util"
+	"github.com/zk-org/zk/internal/util/errors"
+	"github.com/zk-org/zk/internal/util/opt"
+	strutil "github.com/zk-org/zk/internal/util/strings"
 )
 
 // Server holds the state of the Language Server.
@@ -340,7 +340,6 @@ func NewServer(opts ServerOpts) *Server {
 	}
 
 	handler.WorkspaceExecuteCommand = func(context *glsp.Context, params *protocol.ExecuteCommandParams) (interface{}, error) {
-
 		openNotebook := func() (*core.Notebook, error) {
 			args := params.Arguments
 			if len(args) == 0 {
